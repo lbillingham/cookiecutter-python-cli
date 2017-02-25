8fc16bae0cead25ced6b0b4a1ec4737e88ddaf38
Laurence Billingham <lbillingham@users.noreply.github.com>
2017-02-25 16:00:35 +0000

    'help' on non-option argument

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli.py
@@ -3,8 +3,10 @@ import click
 
 @click.command()
 @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False, help='Name of person to greet')
+@click.argument('name', default='world', required=False, metavar='<name>')
 def main(name, as_cowboy):
-    """{{ cookiecutter.project_short_description }}"""
+    """
+    {{ cookiecutter.project_short_description }}
+    """
     greet = 'Howdy' if as_cowboy else 'Hello'
     click.echo('{0}, {1}.'.format(greet, name))
